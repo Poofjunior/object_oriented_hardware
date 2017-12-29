fb0c55552144ff140e55440138123509020b3a13
Joshua Vasquez <jvasquez@g.hmc.edu>
2017-12-28 21:22:36 -0800

    adding hardware interface class

---
+++ hardware_interfaces.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+import abc
+
+class HardwareInterface(object):
+    simulated = False
+
+class DigitalInputInterface(HardwareInterface, metaclass=abc.ABCMeta):
+
+    def __init__(self):
+        self.simulated_input = False
+
+    def read(self):
+        """
+        returns True or False
+        """
+        if self.simulated:
+            return self.simulated_input
+        else:
+            return self.__read()
+
+    @abc.abstractmethod
+    def __read(self):
+        """
+        Performs the actual read from the hardware. Implementation is hardware-specific.
+        """
+        pass
+
+class AnalogInputInterface(HardwareInterface, metaclass=abc.ABCMeta):
+
+    def __init__(self):
+        self.simulated_input = 0.0
+
+    def read(self):
+        """
+        returns the analog value. Units are impolementation-specific.
+        """
+        if self.simulated:
+            return self.simulated_input
+        else:
+            return self.__read()
+
+    @abc.abstractmethod
+    def __read(self):
+        """
+        Performs the actual read from the hardware. Implementation is hardware-specific.
+        """
+        pass
+
+
+class VoltageInputInterface(AnalogInputInterface):
+
+    @abc.abstractmethod
+    def __read(self):
+        """
+        returns an analog voltage in volts.
+        """
+        pass
+
+
+class DigitalOutputInterface(HardwareInterface, metaclass=abc.ABCMeta):
+
+    def __init__(self):
+        self.simulated_output = False
+
+    def write(self, value):
+        """
+        Writes value to the output.
+
+        :param value: True or False
+        """
+        if self.simulated:
+            self.simulated_output = value
+        else:
+            self.__write(value)
+
+    @abc.abstractmethod
+    def __write(self, value):
+        """
+        Performs the actual write to hardware. Implementation is hardware-specific.
+        """
+        pass
+
+
+class AnalogOutputInterface(HardwareInterface, metaclass=abc.ABCMeta):
+
+    def __init__(self):
+        self.simulated_output = 0.0
+
+    def write(self, value):
+        """
+        Writes the analog value to the output. Units are implementation-specific.
+
+        :param value: True or False
+        """
+        if self.simulated:
+            self.simulated_output = value
+        else:
+            self.__write(value)
+
+    @abc.abstractmethod
+    def __write(self, value):
+        """
+        Performs the actual write to hardware. Implementation is hardware-specific.
+        """
+        pass
+
--- temperature_sensors.py
+++ temperature_sensors.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+
 import abc
 from math import log
 
 class TemperatureSensor(metaclass=abc.ABCMeta):
     """
-    an abstract pressure sensor class
+    an abstract temperature sensor class
     """
 
     ABSOLUTE_ZERO_OFFSET_C = 273.15
@@ -39,19 +41,30 @@ class Thermistor(TemperatureSensor):
           a supplementary pulldown resistor added. Pullup value must be provided on init.
     """
 
-    def __init__(self, a, b, c, r2, vin, analog_input_interface):
+    def __init__(self, a, b, c, r2_ohms, vin_v, voltage_input_interface):
+        """
+        Initialize a Thermistor in voltage divider configuration.
+        Note: thermistor is set as the pullup resistor in the voltage divider.
+
+        :param a: Steinhart–Hart equation constant A
+        :param b: Steinhart–Hart equation constant B
+        :param c: Steinhart–Hart equation constant C
+        :param r2_ohms: pulldown resistor resistance in ohms
+        :param vin_v: resistor divider input voltage in volts
+        :param voltage_input_interface: analog input where the voltage-divider input will be measured
+        """
         self.a = a
         self.b = b
         self.c = c
-        self.r2 = r2
-        self.vin = vin
-        self.analog_input = analog_input_interface
+        self.r2_ohms = r2_ohms
+        self.vin_v = vin_v
+        self.analog_input = voltage_input_interface
 
     def _read_resistance(self, voltage_v):
         """
-        returns the resistance of the thermistor.
+        returns the resistance of the thermistor in ohms.
         """
-        return self.r2 * (self.vin/voltage_v - 1)
+        return self.r2_ohms * (self.vin_v/voltage_v - 1)
 
     def read_temperature_k(self):
         """
@@ -68,15 +81,22 @@ class AnalogTemperatureSensor(TemperatureSensor):
     a linear analog-output-based temperature sensor.
     """
 
-    def __init__(self, v1_v, t1_c, v2_v, t2_c, analog_input_interface):
+    def __init__(self, v1_v, t1_c, v2_v, t2_c, voltage_input_interface):
         """
-        temperature units are in Celsius since datasheets tend to prefer it over Kelvin.
+        Establishing the voltage-to-pressure measurement requires two datapoints:
+        (v1_v, t1_c) and (v2_v, t2_c), which can be derived from the sensor specs.
+        Temperature units are in Celsius since datasheets tend to prefer it over Kelvin.
+
+        :param v1_v: voltage measurement (in volts) corresponding to t1_c
+        :param t1_c: temperature measurement (in Celsius) corresponding to v1_v
+        :param v2_v: voltage measurement (in volts) corresponding to t2_c
+        :param t2_c: temperature measurement (in Celsius) corresponding to v2_v
+        :param voltage_input_interface: the analog input from which the voltage will be measured
         """
-        # generate m and b for line formula based on two data points:
-        # (t1, v1) and (t2, v2).
+        # generate slop and y-intercept for line formula based on two data points:
         self.gain = (t1_c - t2_c)/(v1_v - v2_v)
         self.offset = self.gain * (0 - v1_v) + t1_c
-        self.analog_input = analog_input_interface
+        self.analog_input = voltage_input_interface
 
     def read_temperature_c(self):
         """
